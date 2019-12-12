ebe37d14410e8f4af47f5c912fad4f017a72b017
Hevin <hevin@dano.me>
2019-12-12 20:37:59 +0900

    State 관리 대부분 된 상태. UI와 붙이기 전.

--- Constants.js
+++ Constants.js
@@ -4,3 +4,8 @@ export const MAX_DISTANCE = 1000
 export const MAX_LOVE_POINT = 200
 export const THRESHOLD_BEST = 200
 export const THRESHOLD_BETTER = 100
+
+export const WALKING_STOPPER_MILLISECONDS = 3000
+export const GETTING_THIRSTY_MILLISECONDS = 15000
+// 시연을 위해 100%
+export const CHANCE_TO_GET_THIRSTY = 1
